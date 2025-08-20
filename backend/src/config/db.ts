1413cb6b2b3963001796838bcb7c9f6e0b2afbac
22120217 <hoanglenam0905@gmail.com>
2025-08-20 13:24:31 +0700

    fix(deploy): resolve minor bug in server.ts for Vercel deployment

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -57,10 +57,7 @@ class Database {
 
     try {
       const connectionOptions = {
-        maxPoolSize: 10, // Connection pool
-        serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-        bufferCommands: false // Disable mongoose buffering
+        maxPoolSize: 10 // Connection pool
       };
 
       await mongoose.connect(ENV.MONGO_URI, connectionOptions);
