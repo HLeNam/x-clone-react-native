e7ea695ac6b9386202413201bfa5da7d31865cba
22120217 <hoanglenam0905@gmail.com>
2025-08-20 13:06:12 +0700

    fix(deploy): resolve minor bug in server.ts for Vercel deployment

---
+++ backend/api/index.ts
@@ -0,0 +1,22 @@
+import app from './../src/app';
+import { ENV } from './../src/config/env';
+
+const PORT = ENV.PORT;
+
+if (ENV.NODE_ENV !== 'production') {
+  const server = app.listen(PORT, () => {
+    console.log(`✅ X Clone started on port ${PORT}`);
+    console.log(`🌍 Environment: ${ENV.NODE_ENV}`);
+  });
+
+  // Handle exit server express with SIGINT signal (Ctrl + C)
+  process.on('SIGINT', () => {
+    server.close(() => {
+      console.log('⚠️ Exit Server Express');
+      // notify.send( ping...)
+    });
+  });
+}
+
+// export for vercel
+export default app;
