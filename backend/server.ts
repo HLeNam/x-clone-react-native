0bed9d68ea2c838827be4eade050a76959708751
22120217 <hoanglenam0905@gmail.com>
2025-08-20 10:52:33 +0700

    chore(deploy): adjust config for Vercel deployment

--- backend/server.ts
+++ backend/server.ts
@@ -3,15 +3,20 @@ import { ENV } from '~/config/env';
 
 const PORT = ENV.PORT;
 
-const server = app.listen(PORT, () => {
-  console.log(`✅ X Clone started on port ${PORT}`);
-  console.log(`🌍 Environment: ${ENV.NODE_ENV}`);
-});
-
-// Handle exit server express with SIGINT signal (Ctrl + C)
-process.on('SIGINT', () => {
-  server.close(() => {
-    console.log('⚠️ Exit Server Express');
-    // notify.send( ping...)
+if (ENV.NODE_ENV !== 'production') {
+  const server = app.listen(PORT, () => {
+    console.log(`✅ X Clone started on port ${PORT}`);
+    console.log(`🌍 Environment: ${ENV.NODE_ENV}`);
   });
-});
+
+  // Handle exit server express with SIGINT signal (Ctrl + C)
+  process.on('SIGINT', () => {
+    server.close(() => {
+      console.log('⚠️ Exit Server Express');
+      // notify.send( ping...)
+    });
+  });
+}
+
+// export for vercel
+export default app;
